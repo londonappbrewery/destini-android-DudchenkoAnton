63a524d6da403320629c2984485f8ea0955aeaa3
Anton <didchenkoanton94@gmail.com>
2019-10-03 19:03:47 +0300

    a

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,5 +1,6 @@
 package com.londonappbrewery.destini;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,17 +27,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState != null){
+            currentStation = savedInstanceState.getInt("currentStation");
+        } else {
+            currentStation = 1;
+        }
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
         storyText = (TextView) findViewById(R.id.storyTextView);
         upperButton = (Button) findViewById(R.id.buttonTop);
         lowerButton = (Button) findViewById(R.id.buttonBottom);
-        currentStation = 1;
-
         upperButtonPaths = new HashMap<Integer, Integer>();
         lowerButtonPaths = new HashMap<Integer, Integer>();
         idsDictionary = new HashMap<Integer, Integer[]>();
 
+
+        fillDictionaries();
+        setContentToView(currentStation);
+
+        Integer[] integers = idsDictionary.get(currentStation);
+        if(integers[integers.length - 1] != 1)
+            setGameButtonListers();
+    }
+
+    private void setGameButtonListers(){
+        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        upperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentStation = upperButtonPaths.get(currentStation);
+                setContentToView(currentStation);
+
+            }
+        });
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        lowerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentStation = lowerButtonPaths.get(currentStation);
+                setContentToView(currentStation);
+            }
+        });
+    }
+
+    private void setContentToView(int stationNumber){
+
+        Integer[] questionInfoArray = idsDictionary.get(stationNumber);
+        storyText.setText(questionInfoArray[0]);
+
+        if(questionInfoArray[questionInfoArray.length - 1] == 0){
+            upperButton.setText(questionInfoArray[1]);
+            lowerButton.setText(questionInfoArray[2]);
+        }
+        else{
+            setRestartListener(upperButton);
+            setExitListener(lowerButton);
+        }
+    }
+
+    private void setRestartListener(Button restartButton){
+        restartButton.setText(R.string.Restart);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentStation = 1;
+                setGameButtonListers();
+                setContentToView(currentStation);
+            }
+        });
+    }
+
+    private void setExitListener(Button exitButton){
+        exitButton.setText(R.string.Exit);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+    }
+
+    private void fillDictionaries(){
         upperButtonPaths.put(1, 3);
         upperButtonPaths.put(3, 6);
         upperButtonPaths.put(2, 3);
@@ -51,49 +123,11 @@ public class MainActivity extends AppCompatActivity {
         idsDictionary.put(4, new Integer[] {R.string.T4_End, 1});
         idsDictionary.put(5, new Integer[] {R.string.T5_End, 1});
         idsDictionary.put(6, new Integer[] {R.string.T6_End, 1});
+    }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-        upperButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Integer newStation = upperButtonPaths.get(currentStation);
-                Integer[] questionInfoArray = idsDictionary.get(newStation);
-                currentStation = newStation;
-                storyText.setText(questionInfoArray[0]);
-
-                if(questionInfoArray[questionInfoArray.length - 1] == 0){
-                    upperButton.setText(questionInfoArray[1]);
-                    lowerButton.setText(questionInfoArray[2]);
-                }
-                else{
-                    upperButton.setVisibility(View.GONE);
-                    lowerButton.setVisibility(View.GONE);
-                }
-
-            }
-        });
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-        lowerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Integer newStation = lowerButtonPaths.get(currentStation);
-                Integer[] questionInfoArray = idsDictionary.get(newStation);
-                currentStation = newStation;
-                storyText.setText(questionInfoArray[0]);
-
-                if(questionInfoArray[questionInfoArray.length - 1] == 0){
-                    upperButton.setText(questionInfoArray[1]);
-                    lowerButton.setText(questionInfoArray[2]);
-                }
-                else{
-                    upperButton.setVisibility(View.GONE);
-                    lowerButton.setVisibility(View.GONE);
-                }
-            }
-        });
-
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("currentStation", currentStation);
     }
 }
